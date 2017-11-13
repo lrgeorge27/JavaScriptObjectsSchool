617eda9a7d6f34fd99924983dc84c4a08f1b199d
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-13 16:18:27 +0000

    school_object.js

---
+++ add.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+add(4, 9);
---
+++ school_object.js
@@ -0,0 +1,60 @@
+school.classes[1].students[1].grade;
+school.classes[0].year;
+var school = {
+    name: "CareerDevs",
+    twitter: "@CareerDevs",
+    classes: [
+        {
+            name: "Fall 2017 Morning Intro",
+            time: "Morning",
+            type: "Intro",
+            year: 2017,
+            semester: 2,
+            students: [
+                {
+                    name: "Dara Hoy",
+                    grade: 9
+                },
+                {
+                    name: "Lauren George",
+                    grade: 8
+                }
+            ]
+        },
+        {
+            name: "Fall 2017 Evening Intro",
+            time: "Evening",
+            type: "Intermediate",
+            year: 2017,
+            semester: 2,
+            students: [
+                {
+                    name: "Josh Bristol",
+                    grade: 10
+                },
+                {
+                    name: "Matt Curcio",
+                    grade: 7
+                }
+            ]
+        }
+    ],
+    
+    addStudent: function(classNum, studentName, studentGrade) {
+        this.classes[classNum].students.push({
+            name: studentName,
+            grade: studentGrade
+        });
+    },
+    
+    getAvgGrade: function(classNum) {
+        // grab the students array from the class number given.
+        var theStudents = this.classes.students //fill out dot notation
+        var total = 0
+        for(var i = 0; i < theStudents.length; i++) {
+            // we need to get the grade of each student. what Item do we use here?
+            total += theStudents[i]
+        }
+        return total / theStudents.length;
+    }
+}
